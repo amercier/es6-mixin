6181b3c506c542ded562a666e540edfe5f3775db
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-03-07 17:00:03 -0800

    Replaced lodash.topairs by custom implementation

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,13 @@
-import toPairs from 'lodash.topairs';
+/**
+ * Converts an object to an array of pairs, in the form of an array of tuples
+ * where index 0 is the key and index 1 is the value. This include both
+ * enumerable and non-enumerable properties.
+ * @param {object} object Object for which to get pairs
+ * @returns {[string, *][]} Array containing object pairs
+ */
+function entries(object) {
+  return Object.getOwnPropertyNames(object).map(key => [key, object[key]]);
+}
 
 /**
  * Generic mixin superclass. This class is intended to be extended by actual
@@ -41,7 +50,7 @@ import toPairs from 'lodash.topairs';
  *
  *     class FooMixin extends Mixin {
  *       static mixin(target = {}) {
- *         return Mixin.mixin(target, Foo);
+ *         return super.mixin(target, Foo);
  *       }
  *     }
  *
@@ -53,16 +62,17 @@ export default class Mixin {
    * Mixes in this class's methods into an existing object.
    * @param {object} [target={}] Any object to mix this class's methods into
    * @param {object} [MixedIn=this] Constructor to be mixed in
-   * @param {...*} [...args] Arguments to pass to the mixed in constructor, if any
-   * @returns {this} The original target object, mutated
+   * @param {...*} [args] Arguments to pass to the mixed in constructor, if any
+   * @return {object} The original target object, mutated
    */
   static mixin(target = {}, MixedIn = this, ...args) {
     const instance = new MixedIn(...args);
 
     // Get all the methods from this class, bind them to the instance, and copy
     // them to the target object.
-    toPairs(MixedIn)
-      .filter(([, method]) => typeof method === 'function')
+    entries(MixedIn.prototype)
+      .filter(([methodName, method]) =>
+        typeof method === 'function' && methodName !== 'constructor')
       .forEach(([methodName, method]) => (target[methodName] = method.bind(instance)));
 
     return target;
